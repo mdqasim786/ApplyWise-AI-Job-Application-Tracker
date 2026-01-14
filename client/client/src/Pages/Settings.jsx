0f75968f81413eba39bbb3968e29ad28276b1dd7
white-devil5911 <muhammadqasim5911@gmail.com>
2026-01-14 22:22:58 +0500

    added applywise and developer note in settings

--- client/client/src/Pages/Settings.jsx
+++ client/client/src/Pages/Settings.jsx
@@ -2,8 +2,9 @@ import { Download, Trash2} from 'lucide-react';
 
 function Settings(){
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-600 via-blue-400 to-blue-200 p-8">
-      <h1 className="text-3xl ml-50 font-bold">Settings</h1>
+    <div className="min-h-screen bg-gradient-to-b from-blue-500 via-blue-400 to-blue-200 p-8">
+      <h1 className="text-3xl ml-50 font-bold text-white">Settings
+      </h1>
         <div className="mt-6 ml-50 align-center w-3xl p-6 bg-white rounded-lg shadow-md">
           <label className="text-md text-black">Email Address</label>
             <input
@@ -53,6 +54,33 @@ function Settings(){
                 <Trash2 size={16} />
               </button>
             </div>
+        </div>
+
+
+        <div className="mt-6 ml-50 w-3xl p-6 bg-white rounded-lg shadow-md">
+          <h2 className="text-xl font-bold text-gray-800 mb-4">About ApplyWise
+          </h2>
+
+          
+          <div className="flex justify-between items-center py-3 border-b border-gray-200">
+            <span className="text-md text-gray-700">App Version</span>
+            <span className="text-md font-semibold text-gray-900">v1.0.0</span>
+          </div>
+          <div className="flex justify-between items-center py-3 border-b border-gray-200">
+            <span className="text-md text-gray-700">Last Updated</span>
+            <span className="text-md font-semibold text-gray-900">January, 2026</span>
+          </div>
+
+
+          <div className="mt-5">
+            <h3 className="text-sm font-semibold  text-blue-500 uppercase tracking-wider mb-2">Developer Note</h3>
+            <p className="text-md text-gray-700 leading-relaxed">
+            ApplyWise was designed and built from the ground up by <strong>MD Qasim</strong> as a solo 
+            developer project. My mission is to create a seamless, efficient platform that simplifies 
+            the application process for everyone. Every feature is carefully crafted with a focus on 
+            user experience and continuous improvement.
+            </p>
+          </div>
         </div>
     </div>
   )
