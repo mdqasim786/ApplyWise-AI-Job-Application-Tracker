3c9a362dfd53d069fcfe4f8b18520e4945a97608
white-devil5911 <muhammadqasim5911@gmail.com>
2026-01-14 22:02:28 +0500

    created a rough draft form for settings

--- client/client/src/Pages/Settings.jsx
+++ client/client/src/Pages/Settings.jsx
@@ -1,8 +1,52 @@
+import { Download } from 'lucide-react';
+
 function Settings(){
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-semibold mb-6">Settings Page</h1>
-      <p className="text-gray-600">This is where user settings will be managed.</p>
+    <div className="min-h-screen bg-gradient-to-b from-blue-600 via-blue-400 to-blue-200 p-8">
+      <h1 className="text-3xl ml-50 font-bold">Settings</h1>
+        <div className="mt-6 ml-50 align-center w-3xl p-6 bg-white rounded-lg shadow-md">
+          <label className="text-md text-black">Email Address</label>
+            <input
+              disabled
+              className="w-full mt-2 p-2 mb-4 border rounded-xl bg-gray-200 text-gray-600 cursor-not-allowed"
+            />
+
+            <label className="text-md text-black">Add an Alternate Email (Optional)</label>
+              <input
+                type="email"
+                placeholder="Add another email"
+                className="w-full mt-2 p-2 mb-4 border rounded-xl bg-gray-200 text-gray-600"
+            />
+
+            <label className="text-md text-black">Phone Number (Optional)</label>
+              <input
+                type="tel"
+                placeholder="Enter Phone Number"
+                className="w-full mt-2 p-2 mb-4 border rounded-xl bg-gray-200 text-gray-600"
+            />
+
+            <label className="text-md text-black">Add your Resume/CV</label>
+              <input
+                type="file"
+                className="w-full mt-2 p-2 mb-4 border rounded-xl bg-gray-200 text-gray-600"
+            />
+
+            
+            <label className="text-md text-black">Report a bug (Enter details below)</label>
+              <textarea
+                rows="5"
+                type="text"
+                placeholder="Describe the issue in detail..."
+                className="w-full mt-2 p-2 mb-4 border rounded-xl bg-gray-200 text-gray-600"
+            />
+
+            <div className="flex">
+              <label className="text-md font-bold text-black">Download my ApplyWise Profile</label>
+              <button className="ml-auto bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+                <Download size={16} />
+              </button>
+            </div>
+        </div>
     </div>
   )
 }
